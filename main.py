35d819df5cddd69a00f811876ced9abbfb930416
Faris Faikar Razannafi <farisfrontiers@gmail.com>
2021-09-13 19:13:38 +0700

    Um, I made a print_a_smiley_face() method. Idk

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Program:
 
     def run(self):
         self.print_hi()
-        print(self.calculate())
+        self.print_a_smiley_face()
 
     def calculate(self):
         return pow(self.rnd_num, 2) * self.rnd_num
@@ -17,6 +17,12 @@ class Program:
         print(f'Hi, {self.name}')
         print(f"This is a github testing program spesifically used for Pycharm if possibe")
         print("Anyway, here's a random number for no reason:")
+        print(self.calculate())
+
+    def print_a_smiley_face(self):
+        print("Now I will print a smiley face")
+        print(":)")
+        print("Also, here's the random number: " + str(self.rnd_num))
 
 
 if __name__ == '__main__':
